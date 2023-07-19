c5fa2d350f2d2a034df779eb5d0e346c5987dd86
Liliána Bohus <boli96@gmail.com>
2023-07-19 19:53:23 +0200

    add search methods

--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,69 @@
 import java.util.ArrayList;
 
 import java.util.List;
+import java.util.Map;
 
 public class Library {
 
-    private List<Book> books;
+    // TODO
+    // private Map<Book, Integer> bookAmount;
+
+    private final List<Book> books;
+    private final List<Reader> readers;
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<Reader> getReaders() {
+        return readers;
+    }
 
     public Library() {
         this.books = new ArrayList<>();
+        this.readers = new ArrayList<>();
     }
 
     public void addBook(Book book) {
         this.books.add(book);
     }
 
-    // TODO
+    public void addReader(Reader reader) {
+        this.readers.add(reader);
+    }
+
+
     public List<Book> findBookByTitle(String title) {
-        return null;
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public List<Book> findBookByGenre(Genre genre) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getGenre() == genre) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public List<Book> findBooksByAuthor(String authorName) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            for (Author author: book.getAuthors()) {
+                if (author.getName().contains(authorName)) {
+                    result.add(book);
+                    break; // lehet, hogy több szerzője van, amire igaz a keresési feltétel -> csak 1x kerüljön bele
+                }
+            }
+        }
+        return result;
     }
 
 
--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -3,15 +3,27 @@ import java.util.HashSet;
 import java.util.List;
 
 public class LibraryManager {
-
     public static void main(String[] args) {
         Library konyvtar = new Library();
+        initialize(konyvtar);
 
+        System.out.println(konyvtar.findBookByTitle("1984"));
+        System.out.println(konyvtar.findBooksByAuthor("Soros"));
+        System.out.println(konyvtar.findBookByGenre(Genre.SCI_FI));
+    }
+
+    private static void initialize(Library konyvtar) {
         Book konyv = new Book("1984", 1949, Genre.SCI_FI, List.of(new Author("George Orwell")));
+        Book konyv2 = new Book("Migránsok", 2018, Genre.HORROR,
+                List.of(new Author("George Orwell"), new Author("Soros György")));
+        Book konyv3 = new Book("Ez veszélyes", 2018, Genre.SCI_FI,
+                List.of(new Author("Soros György"), new Author("Soros Bérenc")));
         konyvtar.addBook(konyv);
+        konyvtar.addBook(konyv2);
+        konyvtar.addBook(konyv3);
 
         Reader olvaso = new Reader("Andris", "ASD420",
                 new HashSet<>(Arrays.asList(Genre.HORROR, Genre.THRILLER)));
-
+        konyvtar.addReader(olvaso);
     }
 }
